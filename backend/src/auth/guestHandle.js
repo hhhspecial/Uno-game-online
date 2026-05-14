16b4a4803ed7749abeb5fa3d4f6c9a186c9c5e7d
Hoag-afk <nguyenbuihoang2004@gmail.com>
2026-05-14 10:26:51 +0700

    Update Lobby
    add API quick game, update code

--- backend/src/auth/guestHandle.js
+++ backend/src/auth/guestHandle.js
@@ -1,34 +1,53 @@
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcrypt');
 
 const users = {};
 
 const createGuestUser = async () => {
     const id = 'guest_' + uuidv4().slice(0, 8); // Generate a unique guest ID
-    const guestUsername = `guest_` + Math.floor(Math.random() * 10000 +100); // Generate a random guest username
-    const player = {id: id, name: guestUsername, isGuest: true};
+    const guestUsername = `guest_` + Math.floor(Math.random() * 10000 + 100); // Generate a random guest username
+    const player = { id: id, name: guestUsername, isGuest: true };
     users[id] = player;
     return player;
 };
 
 async function register(username, password) {
-    const existingUser = Object.values(users).find(u => !u.isGuest && u.username === username);
+    const existingUser = Object.values(users).find(
+        u => !u.isGuest && u.name === username
+    );
     if (existingUser) throw new Error('Username already exists');
 
     const id = 'user_' + uuidv4().slice(0, 8);
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = {id: id, username: username, password: hashedPassword, isGuest: false};
+    const newUser = {
+        id,
+        name: username,
+        password: hashedPassword,
+        isGuest: false
+    };
+
     users[id] = newUser;
-    return { success: true, userId: id };
+
+    return {
+        id: newUser.id,
+        name: newUser.name,
+        isGuest: false
+    };
 }
 
+
 async function login(username, password) {
-    const existingUser = Object.values(users).find(u => !u.isGuest && u.username === username);
+    const existingUser = Object.values(users).find(u => !u.isGuest && u.name === username);
     if (!existingUser) throw new Error('User not found');
 
     const passwordMatch = await bcrypt.compare(password, existingUser.password);
     if (!passwordMatch) throw new Error('Invalid password');
-    return { success: true, userId: existingUser.id, username: existingUser.username };
+    return {
+        id: existingUser.id,
+        name: existingUser.name,
+        isGuest: false
+    };
+
 }
 
 function getPlayer(id) {
